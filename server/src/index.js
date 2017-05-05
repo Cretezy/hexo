af14a2f6c5c82f0c2f80467dc6418800b71cf5c6
Charles Crete <cretezy@gmail.com>
2017-05-05 15:48:05 -0400

    remove ssl

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,9 @@
 require('dotenv').config({path: '../.env'});
 
-const nodeshout = require("nodeshout");
 const express = require("express");
 const path = require("path");
 const EventEmitter = require('events');
 const httpProxy = require('http-proxy');
-const greenlock = require('greenlock-express');
 
 const app = express();
 
@@ -13,7 +11,6 @@ const state = {
     app,
     events: new EventEmitter(),
 };
-
 
 
 const proxy = httpProxy.createProxyServer({});
@@ -36,20 +33,10 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
         res.sendFile(path.resolve(__dirname, '..', '..', 'web', 'build', 'index.html'));
     });
 }
-if (process.env.USE_SSL === 'true') {
-    server = greenlock.create({
-        server: process.env.SSL_SERVER,
-        email: process.env.SSL_EMAIL,
-        agreeTos: true,
-        approvedDomains: process.env.SSL_DOMAINS.split(","),
-        app,
-        debug: process.env.DEBUG
-    }).listen(process.env.PORT || 80, process.env.PORT_SSL || 443);
-} else {
-    server = require('http').createServer(app);
 
-    server.listen(process.env.PORT || 9000);
-}
+server = require('http').createServer(app);
+
+server.listen(process.env.PORT || 9000);
 
 
 state.io = require('socket.io')(server);
