c6dbc4f6727c573164d8b0985fcff3f648ec58bf
Charles Crete <cretezy@gmail.com>
2017-05-26 15:48:58 -0400

    update react scripts, change to pwa

--- web/src/index.js
+++ web/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'mini.css/dist/mini-dark.min.css';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+registerServiceWorker();
